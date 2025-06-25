69dc970e038c0328670445a7a3300980a287e4b2
vextroyer <victormanuel.vena@gmail.com>
2025-06-25 18:18:02 -0500

    Fix bug on make_random_enemy_move

--- examples/Grab that coin/main.py
+++ examples/Grab that coin/main.py
@@ -62,7 +62,7 @@ def make_optimal_enemy_move(nim_instance: [int]):
 def make_random_enemy_move(nim_instance: [int]):
     non_zero = list(filter(lambda x: x > 0,nim))
     index = random.randint(0,len(non_zero) - 1)
-    value = random.randint(1,non_zero[index])
+    value = random.randint(0,non_zero[index]-1)
     for i in range(len(nim_instance)):
         if nim_instance[i] == non_zero[index]:
             nim_instance[i] = value
